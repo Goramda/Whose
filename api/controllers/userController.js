4c7a4b683398a0a3e9c3fb282d70d52ac77d33a1
Goramda <goddy_2556@hotmail,com>
2018-12-15 00:47:37 +0700

    Login

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -2,11 +2,45 @@ const mongoose = require("mongoose");
 const userSchema = require('../model/userModel')
 const bcrypt = require("bcrypt-nodejs")
 const salt = bcrypt.hashSync("Whose")
+const jwt = require('jsonwebtoken')
 
 exports.login = (req,res,next)=>{
-    res.status(200).json({
-        message : "Login Success"
-    })
+    userSchema
+        .find({email : req.body.email})
+        .exec()
+        .then(user=>{
+           if(user.length < 1){
+                return res.status(401).json({
+                    message : "User not found"        
+                })
+           }
+           bcrypt.compare(req.body.password , user[0].password , (err , result)=>{
+               if(err){
+                   return res.status(401).json({
+                       message : "Wrong Password"
+                   })
+               }if(result){
+                   const token = jwt.sign({
+                        email : user[0].email,
+                        userId : user[0]._id
+                   } , "Secret_Key" , { expiresIn : "1h"})
+                   return res.status(200).json({
+                       message : "Auth successfully",
+                       token : token,
+                       fName : user[0].fullName,
+                       lName : user[0].lName,
+                        _id : user[0]._id
+                    })   
+               }
+               res.status(401).json({
+                   message : "Auth Failed"
+               })
+           })
+        }).catch(err=>{
+            res.status(500).json({
+                message : "Something went wrong"
+            })
+        })
 }
 
 exports.register = (req,res,next)=>{
@@ -29,7 +63,7 @@ exports.register = (req,res,next)=>{
                         const User = new userSchema({
                             _id : new mongoose.Types.ObjectId(),
                             email : req.body.email,
-                            password : req.body.password,
+                            password : hash,
                             fName : req.body.fName,
                             lName : req.body.lName
                             
@@ -37,9 +71,15 @@ exports.register = (req,res,next)=>{
                         User
                             .save()
                             .then(result =>{
+                                const token = jwt.sign({
+                                    email : req.body.email,
+                                    userId : User._id
+                               } , "Secret_Key" , { expiresIn : "1h"})
                                 res.status(201).json({
                                     message : "Create user successfully",
+                                    token ,
                                     result  
+
                                 })
                             }).catch(err=>{
                                 console.log(err)
